46a8ece49e5fb6097e588c5f3e685aa8a6450c75
Gowtham Sankar Gunasekaran <ggowthamsankar@gmail.com>
2022-05-01 17:31:30 +0530

    Update

--- src/main/java/learn/ggs/springboot/java/springboot/SpringbootLearn1Application.java
+++ src/main/java/learn/ggs/springboot/java/springboot/SpringbootLearn1Application.java
@@ -2,10 +2,10 @@ package learn.ggs.springboot.java.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootLearn1Application  {    //extends SpringBootServletInitializer
+public class SpringbootLearn1Application extends SpringBootServletInitializer  {    //extends SpringBootServletInitializer
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootLearn1Application.class, args);
